74c89c7727b1b537a7ecb3b5ba3dcaef2ee0a9e6
Kristen <kgladfelt@gmail.com>
2017-11-01 18:30:20 -0600

    hardcoded routing table to see how it works, works from client1->routerA->routerB->routerD->Server1

--- simulation.py
+++ simulation.py
@@ -13,7 +13,7 @@ from time import sleep
 router_queue_size = 0  # 0 means unlimited
 
 # may need to increase...
-simulation_time = 1  # give the network sufficient time to transfer all packets before quitting
+simulation_time = 5  # give the network sufficient time to transfer all packets before quitting
 
 if __name__ == '__main__':
     object_L = []  # keeps track of objects, so we can kill their threads
@@ -26,9 +26,21 @@ if __name__ == '__main__':
     server1 = network.Host(2)
     object_L.append(server1)
 
+    #adding another server
+    client2 = network.Host(3)
+    server3 = network.Host(4)
+    object_L.append(client2)
+    object_L.append(server3)
+
     # add routers to list
     router_a = network.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    router_b = network.Router(name='B', intf_count=1, max_queue_size=router_queue_size)
+    router_c = network.Router(name='C', intf_count=1, max_queue_size=router_queue_size)
+    router_d = network.Router(name='D', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_a)
+    object_L.append(router_b)
+    object_L.append(router_c)
+    object_L.append(router_d)
 
     # create a Link Layer to keep track of links between network nodes
     # add LinkLayer to list
@@ -37,8 +49,12 @@ if __name__ == '__main__':
 
     # add all the links to the LinkLayer
     # specify the mtu at the end.
-    link_layer.add_link(link.Link(client1, 0, router_a, 0, 50))
-    link_layer.add_link(link.Link(router_a, 0, server1, 0, 30))
+    #second and fourth element pertains to the interface, changing them to work with routing table
+    link_layer.add_link(link.Link(client1, 0, router_a, 0, 50))     #from client 1 and two on interface 0, forward to router a
+    link_layer.add_link(link.Link(client2, 0, router_a, 0, 50))
+    link_layer.add_link(link.Link(router_a, 0, router_b, 0, 30))
+    link_layer.add_link(link.Link(router_b, 0, router_d, 0, 30))
+    link_layer.add_link(link.Link(router_d, 0, server1, 0, 30))
 
     # this is the minimum of the link layer's mtu's and sent in to the udt for fragment sizing.
     min_mtu = 30
@@ -47,7 +63,12 @@ if __name__ == '__main__':
     # TODO: #3 Start new object threads
     thread_L = [threading.Thread(name=client1.__str__(), target=client1.run),
                 threading.Thread(name=server1.__str__(), target=server1.run),
+                threading.Thread(name=client2.__str__(), target=client2.run),
+                threading.Thread(name=server3.__str__(), target=server3.run),
                 threading.Thread(name=router_a.__str__(), target=router_a.run),
+                threading.Thread(name=router_b.__str__(), target=router_b.run),
+                threading.Thread(name=router_c.__str__(), target=router_c.run),
+                threading.Thread(name=router_d.__str__(), target=router_d.run),
                 threading.Thread(name="Network", target=link_layer.run)]
 
     for t in thread_L:
@@ -59,7 +80,7 @@ if __name__ == '__main__':
     for i in range(43, 123):
         data += chr(i)
 
-    print(data)
+    print('Initial Data: ' + data)
 
     # send message -> destination, source, data
     # client(host 1) sending to server(host 2)
